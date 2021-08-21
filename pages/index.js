12854f7656ae42567fd1d81e5a11fa6ac73e76ea
lynii12 <d0228582@gmail.com>
2021-08-21 11:09:06 +0800

    add loader to read mp3 file

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Lyrics from "../components/Sentence";
+import exampleSong from "../assets/Shaolin_Dub_FootSoldier.mp3";
 
 export default function IndexPage() {
   return (
     <div>
       <audio controls>
-        <source src="horse.mp3" type="audio/mpeg" />
+        <source src={exampleSong} type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
       <Lyrics />
